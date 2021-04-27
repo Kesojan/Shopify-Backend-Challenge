4ac5e6be164a3751cd89c87dcfff63efdcae8c86
Kesojan <keso1429@gmail.com>
2021-04-27 16:39:23 -0400

    Add active storage

---
+++ app/controllers/images_controller.rb
@@ -0,0 +1,21 @@
+class ImagesController < ApplicationController
+    def new 
+        @image = Image.new
+    end
+
+    def create
+        @image = current_user.images.build(image_params)
+        if image.save
+            redirect_to image_path(@image.id), notice: "Image uploaded successfully"
+        else
+            render :new
+
+    end
+end
+
+    private
+
+    def image_params
+        params.require(:image).permit(:image, :description)
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   get 'home/index'
   root 'pages#home'
+  get '/upload', to: 'images#new'
+  resources :images, only: [:index, :show, :create]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
