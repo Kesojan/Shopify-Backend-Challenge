35e3f7dc82e4e4f1bc3fe2293552012575771a79
Kesojan <keso1429@gmail.com>
2021-04-27 23:21:23 -0400

    link multiple pic uploads

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,8 @@
+class UsersController <ApplicationController
+    def show
+        @user = User.find_by(username: params[:username])
+        @images = @user.images.order(created_At: :desc).limit(10)
+    end
+
+end
+
--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,10 @@ Rails.application.routes.draw do
   devise_for :users
   get 'home/index'
   root 'pages#home'
-  
+
   get '/upload', to: 'images#new'
   resources :images, only: [:index, :show, :create]
 
-  get '/@:username', to: 'user#show', as: :profile
+  get '/@:username', to: 'users#show', as: :profile
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
