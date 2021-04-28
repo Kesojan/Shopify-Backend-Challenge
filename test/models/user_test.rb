35d5a271e0fac56ec1d01c9e54a10e9b948f8676
Kesojan <keso1429@gmail.com>
2021-04-28 14:11:39 -0400

    some unit tests

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,16 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should not allow duplicate user names" do
+    user1 = User.create(username: "harvey", password: "lawyer")
+    user2 = User.new(username: "harvey", password: "lawyer")
+    user1.save
+    assert_not user2.save, "Saved a duplicate user name"
+  end
+
+  test "should not allow passwords under 6 characters" do
+    user1 = User.create(username: "harvey", password: "one")
+    user1.save
+    assert_not user1.save, "Saved a invalid password"
+  end
 end
